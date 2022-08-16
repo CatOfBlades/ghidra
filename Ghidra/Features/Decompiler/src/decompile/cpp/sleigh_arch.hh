c6f83ec60a7f5020754529a139c1e7a860787792
CatOfBlades <Fergley@yahoo.com>
2022-08-16 19:18:24 -0400

    Make SleighArchitecture Langs public  by thestr4ng3r

--- Ghidra/Features/Decompiler/src/decompile/cpp/sleigh_arch.hh
+++ Ghidra/Features/Decompiler/src/decompile/cpp/sleigh_arch.hh
@@ -114,7 +114,7 @@ class SleighArchitecture : public Architecture {
 protected:
   ostream *errorstream;					///< Error stream associated with \b this SleighArchitecture
   // buildLoader must be filled in by derived class
-  static void collectSpecFiles(ostream &errs);		///< Gather specification files in normal locations
+
   virtual Translate *buildTranslator(DocumentStorage &store);
   virtual PcodeInjectLibrary *buildPcodeInjectLibrary(void);
   virtual void buildSpecFile(DocumentStorage &store);
@@ -129,7 +129,8 @@ public:
   virtual void printMessage(const string &message) const { *errorstream << message << endl; }
   virtual ~SleighArchitecture(void);
   virtual string getDescription(void) const;
-
+  static void collectSpecFiles(ostream &errs);		///< Gather specification files in normal locations
+  
   static string normalizeProcessor(const string &nm);		///< Try to recover a \e language \e id processor field
   static string normalizeEndian(const string &nm);		///< Try to recover a \e language \e id endianess field
   static string normalizeSize(const string &nm);		///< Try to recover a \e language \e id size field
